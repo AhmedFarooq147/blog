46313393fd7f80cd60b9506ced12d8a04c92b737
Ahmed Farooq <ahmedarain1697@gmail.com>
2025-01-22 21:24:35 +0500

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,55 @@
-
-
-import React from "react"
-
-
-
-
+'use client'
+import React, { useState } from "react"
+import Swal from 'sweetalert2'
 
 
 export default function Contact() {
+    interface FormData {
+        name: string;
+        email: string;
+        subject: string,
+        message: string;
+    }
+    const [name, setname] = useState('')
+    const [email, setemail] = useState('')
+    const [subject, setsubject] = useState('')
+    const [message, setmsg] = useState('')
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const target = e.target as HTMLFormElement;
+        const nameInput = target.elements.namedItem('name') as HTMLInputElement;
+        const emailInput = target.elements.namedItem('email') as HTMLInputElement;
+        const messageInput = target.elements.namedItem('message') as HTMLTextAreaElement;
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                access_key: "420dc55c-d50f-4b77-9fd7-9d3be14721b4",
+
+                name: nameInput.value,
+                email: emailInput.value,
+                message: messageInput.value,
+            }),
+        });
+        const result = await response.json();
+        if (result.success) {
+            console.log(result);
+            Swal.fire({
+                title: "success!",
+                text: "Message sent successfully!",
+                icon: "success"
+            });
+            setemail('');
+            setname('')
+            setsubject('')
+            setmsg('')
+
+        }
+    }
 
     return (
         <div className="leading-6">
@@ -18,27 +60,27 @@ export default function Contact() {
                 <p className="mt-5">
                     If you have any questions, suggestions, or feedback, feel free to fill out the form below or reach out to us directly!
                 </p>
-                <div className="grid grid-rows-4">
-                    <label className="mt-[30px] mb-[5px] font-bold" htmlFor="name">Name:</label>
-                    <input className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="text" placeholder="Your Name" />
-                    <label className="mt-[30px] mb-[5px] font-bold" htmlFor="email" aria-required>Email:</label>
-                    <input className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="email" placeholder="Your Email" />
-                    <label className="mt-[10px] mb-[5px] font-bold" htmlFor="subject">Subject:</label>
-                    <input className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="text" placeholder="Subject" />
-                    <label className="mt-[10px] mb-[5px] font-bold" htmlFor="message">Message:</label>
-                    <textarea className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]"
-                        id="message"
-                        name="message"
-                        placeholder="Your Message"
-                        required
-                    ></textarea>
-                </div>
-                <a href={'/contact'}>
-                   
-                        <button className="py-[10px]  w-full px-[20px] bg-[#007BFF] text-white rounded-[5px] cursor-pointer scale-[0.3s] hover:bg-[#0056b3]  " type="submit">Send Message</button>
-                    </a>
+                <form onSubmit={handleSubmit} >
+                    <div className="grid grid-rows-4">
+                        <label className="mt-[30px] mb-[5px] font-bold" htmlFor="name">Name:</label>
+                        <input value={name} onChange={(e) => { setname(e.target.value) }} name="name" className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="text" placeholder="Your Name" />
+                        <label className="mt-[30px] mb-[5px] font-bold" htmlFor="email" aria-required>Email:</label>
+                        <input value={email} onChange={(e) => { setemail(e.target.value) }} name="email" className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="email" placeholder="Your Email" />
+                        <label className="mt-[10px] mb-[5px] font-bold" htmlFor="subject">Subject:</label>
+                        <input value={subject} onChange={(e) => { setsubject(e.target.value) }} name="subject" className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]" type="text" placeholder="Subject" />
+                        <label className="mt-[10px] mb-[5px] font-bold" htmlFor="message">Message:</label>
+                        <textarea value={message} onChange={(e) => { setmsg(e.target.value) }} className="p-[10px] mb-[15px] border-[1px] border-solid border-[#ddd] rounded-[5px]"
+                            id="message"
+                            name="message"
+                            placeholder="Your Message"
+                            required
+                        ></textarea>
+                    </div>
+                    <button typeof="submit" className="py-[10px]  w-full px-[20px] bg-[#007BFF] text-white rounded-[5px] cursor-pointer scale-[0.3s] hover:bg-[#0056b3]  " type="submit">Send Message</button>
+                </form>
 
             </div>
+
         </div>
     )
 };
